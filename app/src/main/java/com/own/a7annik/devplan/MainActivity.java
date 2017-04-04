72ea5a25ed11f27c950d7c210a49d5909df4a638
Jannik hein <7annik.Hein@gmail.com>
2017-04-04 17:17:44 +0200

    Color changes

--- app/src/main/java/com/own/a7annik/devplan/MainActivity.java
+++ app/src/main/java/com/own/a7annik/devplan/MainActivity.java
@@ -1,7 +1,13 @@
 package com.own.a7annik.devplan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.R.attr.name;
+import static com.own.a7annik.devplan.R.string.app_name;
 
 /*
    Class description:
@@ -17,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void button1_click(View view) {
+        // Toast message floating on the bottom of the screen
+        //   .makeText(sending object, message, duration)
+        Toast.makeText(this, getString(app_name), Toast.LENGTH_SHORT).show();
 
+        // Using an intent to switch the activity from MainActivity to TaskActivity
+        //
+        Intent i = new Intent(this, TaskActivity.class);
+        startActivity(i);
+    }
 }
